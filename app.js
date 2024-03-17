2327b470e7599a84788cc5c03240d79b364b4860
Nathalia <nathaliaserrap@gmail.com>
2024-03-17 18:25:25 -0300

    Alterando valor da variável do numeroSecreto

--- app.js
+++ app.js
@@ -2,5 +2,8 @@
 alert("Boas vindas ao jogo do número secreto");
 
 /*Let - Cria uma váriavel que guarda um valor dentro dela (Os nomes da váriavel devem ser atribuidos de acordo com a sua utilidade) */
-let numeroSecreto = 29;
+let numeroSecreto = 5;
+
+/*Prompt() - O prompt é um pop-up que permite a escrita dentro de uma caixa de texto. Nesse caso o valor escrito no prompt será guardado dentro da váriavel "chute" */
+let chute = prompt("Escolha um número entre 1 e 10");
 
