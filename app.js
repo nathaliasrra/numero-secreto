1a6715193f60541f1f301c493379b1587e76724f
Nathalia <nathaliaserrap@gmail.com>
2024-03-17 18:22:16 -0300

    Criando uma variável para guardar o número secreto

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
+/*Alert() - É uma função do js que exibe um pop-up na tela com uma mensagem */
 alert("Boas vindas ao jogo do número secreto");
+
+/*Let - Cria uma váriavel que guarda um valor dentro dela (Os nomes da váriavel devem ser atribuidos de acordo com a sua utilidade) */
+let numeroSecreto = 29;
+
