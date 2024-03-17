3e09bb1d75925b0a3db16f9541eaa7c5ccb9b084
Nathalia <nathaliaserrap@gmail.com>
2024-03-17 19:08:34 -0300

    Adicionando uma variável para contar as tentativas e adicionando ela dentro do loop

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ let numeroSecreto = 5;
 
 /*Prompt() - O prompt é um pop-up que permite a escrita dentro de uma caixa de texto. Nesse caso o valor escrito no prompt será guardado dentro da váriavel "chute" */
 let chute;
+let tentativas = 1;
 
 //while() - O while serve para executar um bloco de código não for igual ao número secreto (false) ele executará um bloco de código
 while(chute != numeroSecreto) {
@@ -17,14 +18,15 @@ while(chute != numeroSecreto) {
     if(chute == numeroSecreto) { //se o chute for igual ao numeroSecerto
         /*Console.log() - É uma função utilizada para executar testes rápidos, evitando quebrar o próprio código caso nao dê certo 
         O console fica localizado no inspecionar do navegador apertando o botão direito do mouse*/
-        alert(`Você acertou, adivinhou o número secreto! (${numeroSecreto})`); //exiba isso
+        alert(`Você acertou, adivinhou o número secreto (${numeroSecreto}), com ${tentativas} tentativas.`); //exiba isso
 
     } else { //se não for igual
         if(chute > numeroSecreto) { // se co chute for maior que o numeroSecreto
-            alert(`O número secreto é menor que ${chute}`) //exiba isso
+            alert(`O número secreto é menor que ${chute}`); //exiba isso
         } else { // se o chute for menor que o numeroSecreto 
-            alert(`O número secreto é maior que ${chute}`) //exiba isso
+            alert(`O número secreto é maior que ${chute}`); //exiba isso
         }
+        tentativas++;
     }
 
 }
