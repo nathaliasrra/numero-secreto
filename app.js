8db470401106ea044ec32df63e2b09b8af930954
Nathalia <nathaliaserrap@gmail.com>
2024-03-17 19:36:07 -0300

    Adicionando o operador ternário para corrigir o erro de português na palavra tentativa

--- app.js
+++ app.js
@@ -3,22 +3,24 @@ alert("Boas vindas ao jogo do número secreto");
 
 /*Let - Cria uma váriavel que guarda um valor dentro dela (Os nomes da váriavel devem ser atribuidos de acordo com a sua utilidade) */
 let numeroSecreto = 5;
-
-/*Prompt() - O prompt é um pop-up que permite a escrita dentro de uma caixa de texto. Nesse caso o valor escrito no prompt será guardado dentro da váriavel "chute" */
-let chute;
-let tentativas = 1;
+let chute; //o chute vai ser o valor que guardará o que será digitado pelo usuário
+let tentativas = 1; // a variável tentativa vai guardar quantas vezes o usuário demorou para acertar o número secreto
 
 //while() - O while serve para executar um bloco de código não for igual ao número secreto (false) ele executará um bloco de código
 while(chute != numeroSecreto) {
 
+    /*Prompt() - O prompt é um pop-up que permite a escrita dentro de uma caixa de texto. Nesse caso o valor escrito no prompt será guardado dentro da váriavel "chute" */
    chute = prompt("Escolha um número entre 1 e 10");
 
    /*If() - É uma condicional que permite a comparação de dois ou mais valores. O primero bloco permite que execute caso a comparação seja verdade (true), caso seja falso (false) ele executará o bloco do else */
 
     if(chute == numeroSecreto) { //se o chute for igual ao numeroSecerto
+        //a variável palavraTentativa conserta o erro de português, caso o usuário acerte de primeira será necessário colocar a palavra "tentativa" no singular
+        let palavraTentativa = tentativas > 1 ? "tentativas" : "tentativa";
         /*Console.log() - É uma função utilizada para executar testes rápidos, evitando quebrar o próprio código caso nao dê certo 
         O console fica localizado no inspecionar do navegador apertando o botão direito do mouse*/
-        alert(`Você acertou, adivinhou o número secreto (${numeroSecreto}), com ${tentativas} tentativas.`); //exiba isso
+        alert(`Você acertou, adivinhou o número secreto (${numeroSecreto}), com ${tentativas} ${palavraTentativa}.`); //exiba isso
+        break; //isso encerra o loop quando o usuário acertar
 
     } else { //se não for igual
         if(chute > numeroSecreto) { // se co chute for maior que o numeroSecreto
@@ -26,7 +28,7 @@ while(chute != numeroSecreto) {
         } else { // se o chute for menor que o numeroSecreto 
             alert(`O número secreto é maior que ${chute}`); //exiba isso
         }
-        tentativas++;
+        tentativas++; //A contagem de tentativas será colocada fora do else, pois será contada também as vezes que o usuário acertar ou seja, será contado os erros e o acerto
     }
 
 }
