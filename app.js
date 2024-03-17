b995657f3e47c7289a94f9278bd65d7b814a9bfd
Nathalia <nathaliaserrap@gmail.com>
2024-03-17 19:00:29 -0300

    Adiconando a codicional na estrutura do while

--- app.js
+++ app.js
@@ -5,22 +5,27 @@ alert("Boas vindas ao jogo do número secreto");
 let numeroSecreto = 5;
 
 /*Prompt() - O prompt é um pop-up que permite a escrita dentro de uma caixa de texto. Nesse caso o valor escrito no prompt será guardado dentro da váriavel "chute" */
-let chute = prompt("Escolha um número entre 1 e 10");
+let chute;
 
-/*If() - É uma condicional que permite a comparação de dois ou mais valores. O primero bloco permite que execute caso a comparação seja verdade (true), caso seja falso (false) ele executará o bloco do else */
+//while() - O while serve para executar um bloco de código não for igual ao número secreto (false) ele executará um bloco de código
+while(chute != numeroSecreto) {
 
-if(chute == numeroSecreto) { //se o chute for igual ao numeroSecerto
+   chute = prompt("Escolha um número entre 1 e 10");
 
-    /*Console.log() - É uma função utilizada para executar testes rápidos, evitando quebrar o próprio código caso nao dê certo 
-    O console fica localizado no inspecionar do navegador apertando o botão direito do mouse*/
+   /*If() - É uma condicional que permite a comparação de dois ou mais valores. O primero bloco permite que execute caso a comparação seja verdade (true), caso seja falso (false) ele executará o bloco do else */
 
-    alert(`Você acertou, adivinhou o número secreto! (${numeroSecreto})`); //exiba isso
+    if(chute == numeroSecreto) { //se o chute for igual ao numeroSecerto
+        /*Console.log() - É uma função utilizada para executar testes rápidos, evitando quebrar o próprio código caso nao dê certo 
+        O console fica localizado no inspecionar do navegador apertando o botão direito do mouse*/
+        alert(`Você acertou, adivinhou o número secreto! (${numeroSecreto})`); //exiba isso
 
-} else { //se não for igual
-    if(chute > numeroSecreto) {
-        alert(`O número secreto é menor que ${chute}`)
-    } else {
-        alert(`O número secreto é maior que ${chute}`)
+    } else { //se não for igual
+        if(chute > numeroSecreto) { // se co chute for maior que o numeroSecreto
+            alert(`O número secreto é menor que ${chute}`) //exiba isso
+        } else { // se o chute for menor que o numeroSecreto 
+            alert(`O número secreto é maior que ${chute}`) //exiba isso
+        }
     }
+
 }
 
